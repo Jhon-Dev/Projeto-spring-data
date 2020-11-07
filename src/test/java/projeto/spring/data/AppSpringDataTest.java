f9897381509c8247639e003a6844955d337e2310
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-06 21:50:53 -0300

    Realizando consulta no banco de dados

--- src/test/java/projeto/spring/data/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/AppSpringDataTest.java
@@ -1,5 +1,7 @@
 package projeto.spring.data;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +34,18 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testeConsulta() {	
-		System.out.println("Iniciando Spring com sucesso");		
+
+		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(2L);
+		
+		System.out.println(usuarioSpringData.get().getNome());
+		System.out.println(usuarioSpringData.get().getIdade());
+		System.out.println(usuarioSpringData.get().getLogin());
+		System.out.println(usuarioSpringData.get().getSenha());
+		System.out.println(usuarioSpringData.get().getEmail());
+		System.out.println(usuarioSpringData.get().getId());
+
+		
+		
 	}
 	
 }
